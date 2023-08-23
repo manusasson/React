6f5de6cd888d12ea3e1812ba910bd1e6055c153f
Manu <manusasson@gmail.com>
2023-08-23 20:16:35 -0300

    Primera preentrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import{ButtonConParametros} from './components/Button'
+import{Navbar} from './components/NavBar' //importo un boton desde la carpeta button
+import {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Agrega esta línea para importar el CSS de Bootstrap
+
+
+
+
 
 function App() {
+const [counter, setCounter] = useState(0)
+
+  ///defino un evento al onclick
+const handlerOnClick = () => {alert('enviar un mensaje')}
+  ///defino un evento al onclick
+const handlerSetCounter = () => {
+  setCounter(counter +  1)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React con Manu
-        </a>
-      </header>
-    </div>
+   <div>
+    <Navbar/>
+     <Titulo/>
+     <SubTitulo/>
+     <Buttons/>
+     <TituloConPropiedades titulo={"Titulo por Parametros"} />
+     <ButtonConParametros evento={handlerOnClick} text={"primer texto"}/> //llamo al evento al hacer on onClick
+     <br/>
+     <ButtonConParametros evento={handlerSetCounter} text={"contador:"+counter}/> //llamo al evento al hacer on onClick
+   </div>
   );
 }
+
+const Titulo = () => <h1>Titulos</h1>
+const SubTitulo = () => <h2>Sub Titulo</h2>
+const Buttons = () => {return (<button>Boton1</button>)}
+const TituloConPropiedades = ({titulo}) => <h1>{titulo}</h1>
 
 export default App;
---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+
+//para exportar se pone export
+export const ButtonConParametros = ({evento,text}) => { return (
+    <button onClick={evento}>
+      {text}
+    </button>
+  )} 
