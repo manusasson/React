af05f93b16cef6a0328b7eb241e1c2fbd42171e3
Manu <manusasson@gmail.com>
2023-09-20 17:02:04 -0300

    Protected Route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import{Navbar} from './components/NavBar' //importo un boton desde la carpeta button
 import 'bootstrap/dist/css/bootstrap.min.css'; // Agrega esta línea para importar el CSS de Bootstrap
 import {BrowserRouter, Routes, Route}  from "react-router-dom"
+import ProtectedRoute from './ProtectedRoute';
 import Home from "./pages/Home"
 import Products  from "./pages/Products"
 import MyAccount  from "./pages/MyAccount"
-
+import { useState } from 'react';
 
 
 function App() {
+  const [session, setSession] = useState(null)
 
+  const handleSetSession = (user) => {
+    setSession(user)
+  }
 
   return (
    <BrowserRouter>
@@ -18,8 +23,11 @@ function App() {
       <Routes>
         <Route path='/' element ={<Home />} />
         <Route path='/products' element ={<Products />} />
-        <Route path='/account' element ={<MyAccount />} />
-
+        <Route path='/my-account' element={
+            <ProtectedRoute session={session}>
+              <MyAccount onLogout={handleSetSession} user={session} />
+            </ProtectedRoute>
+          } />
       </Routes>
    </BrowserRouter>
   );
