d5b946998940c349126bad5c67b945889c7f87fe
Manu <manusasson@gmail.com>
2023-09-20 22:08:31 -0300

    imagenes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +13,26 @@ root.render(
   </React.StrictMode>
 );
 
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBz1QEA-pDJbhzqWPn41qISBSuIUDe6iaA",
+  authDomain: "cursoreactcoder-8e2fd.firebaseapp.com",
+  projectId: "cursoreactcoder-8e2fd",
+  storageBucket: "cursoreactcoder-8e2fd.appspot.com",
+  messagingSenderId: "916704211717",
+  appId: "1:916704211717:web:24389007b2c90c9698fd6c",
+  measurementId: "G-JE0J68GGN2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
